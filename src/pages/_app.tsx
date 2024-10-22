21eaf1f9a530df8fd951830c149f25d8a5378e52
emrocode <92260849+emrocode@users.noreply.github.com>
2024-10-22 12:26:34 -0300

    Add provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,21 @@
 import "@/styles/globals.css";
+import { PiksProvider } from "@/contexts";
 import Layout from "@/components/Layout";
 import { Toaster } from "sonner";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-      <Toaster
-        toastOptions={{
-          unstyled: true,
-          className: "piksToast",
-        }}
-      />
-    </Layout>
+    <PiksProvider>
+      <Layout>
+        <Component {...pageProps} />
+        <Toaster
+          toastOptions={{
+            unstyled: true,
+            className: "piksToast",
+          }}
+        />
+      </Layout>
+    </PiksProvider>
   );
 }
